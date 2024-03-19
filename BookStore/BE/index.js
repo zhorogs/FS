d842af5de0e708722a3b51e0a2158fe3cceccedb
zhorogs <zgslavchev@gmail.com>
2024-03-20 00:01:48 +0200

    first http route

--- BookStore/BE/index.js
+++ BookStore/BE/index.js
@@ -3,6 +3,12 @@ import { PORT } from "./config.js";
 
 const app = express();
 
+
+app.get('/', (request, response) => {
+    console.log(request)
+    return response.status(234).send(`First Http Route`)
+});
+
 app.listen(PORT, () => {
     console.log(`App is listening on ${PORT}`)
 })
