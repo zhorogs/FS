3382865244a0bdd7f9adee76a038a24761f0d760
zhorogs <zgslavchev@gmail.com>
2024-03-20 23:18:52 +0200

    add counting for books

--- BookStore/BE/index.js
+++ BookStore/BE/index.js
@@ -40,7 +40,10 @@ app.post('/books', async (request, response) => {
 app.get('/books', async (request, response) => {
     try {
         const books = await Book.find({});
-        return response.status(200).json(books);
+        return response.status(200).json({
+            count: books.length,
+            data: books
+        });
     } catch (error) {
         console.log(error.message);
         response.status(500).send({ message: error.message });
