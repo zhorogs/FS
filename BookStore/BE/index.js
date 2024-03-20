e0e6d51c0c67f4592db0376b2ac16258884ddacf
zhorogs <zgslavchev@gmail.com>
2024-03-20 23:17:19 +0200

    get all books with mongoose

--- BookStore/BE/index.js
+++ BookStore/BE/index.js
@@ -37,6 +37,15 @@ app.post('/books', async (request, response) => {
     }
 });
 
+app.get('/books', async (request, response) => {
+    try {
+        const books = await Book.find({});
+        return response.status(200);
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+})
 mongoose
     .connect(mongoDBURL)
     .then(() => {
