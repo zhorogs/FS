d768d9b184536f31a1eca5b652a28cd1abacdd63
zhorogs <zgslavchev@gmail.com>
2024-03-21 23:39:19 +0200

    add update book

--- BookStore/BE/index.js
+++ BookStore/BE/index.js
@@ -7,30 +7,32 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (request, response) => {
+app.get("/", (request, response) => {
     console.log(request)
-    return response.status(234).send(`First Http Route`)
+    return response.status(234).send("First Http Route")
 });
 
-app.post('/books', async (request, response) => {
+app.post("/books", async (request, response) => {
     try {
+
         if (
             !request.body.title ||
             !request.body.author ||
             !request.body.publishYear
         ) {
             return response.status(400).send({
-                message: 'Send all required fields: title, author, publishYear'
+                message: "Send all required fields: title, author, publishYear"
             })
         }
+
         const newBook = {
             title: request.body.title,
             author: request.body.author,
             publishYear: request.body.publishYear
         }
-
         const book = await Book.create(newBook);
         return response.status.status(201).send(book);
+
     } catch (error) {
         console.log(error.message);
         response.status(500).send({ message: error.message });
@@ -38,13 +40,15 @@ app.post('/books', async (request, response) => {
 });
 
 //route to get all books from database
-app.get('/books', async (request, response) => {
+app.get("/books", async (request, response) => {
     try {
+
         const books = await Book.find({});
         return response.status(200).json({
             count: books.length,
             data: books
         });
+
     } catch (error) {
         console.log(error.message);
         response.status(500).send({ message: error.message });
@@ -52,13 +56,42 @@ app.get('/books', async (request, response) => {
 });
 
 //route for getting one book from database by id
-app.get('/books/:id', async (request, response) => {
+app.get("/books/:id", async (request, response) => {
     try {
+
         const { id } = request.params;
         const book = await Book.findById({});
         return response.status(200).json(book);
+
     } catch (error) {
         console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+});
+
+//route to update a book
+app.put("/books/: id", async (request, response) => {
+    try {
+
+        if (
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: "Send all required fileds: title, author, publishYear"
+            })
+        }
+
+        const { id } = request.params;
+        const result = await Book.findByIdAndUpdate(id, request.body);
+        if (!result) {
+            return response.status(404).json({ message: "Book not found" })
+        }
+        return response.status(200).send({ message: "Book updated successfully" });
+
+    } catch (error) {
+        console.log(error.message)
         response.status(500).send({ message: error.message });
     }
 });
