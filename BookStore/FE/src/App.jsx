9f19279ba2f36a3f663b78e0fac6a0fc8911f90e
zhorogs <zgslavchev@gmail.com>
2024-03-23 23:51:16 +0200

    configure pages path and elements

--- BookStore/FE/src/App.jsx
+++ BookStore/FE/src/App.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import CreateBooks from "./pages/CreateBooks";
+import DeleteBook from "./pages/DeleteBook";
+import EditBook from "./pages/EditBooks";
+import ShowBook from "./pages/ShowBook";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="" element={ } />
-      <Route path="" element={ } />
-      <Route path="" element={ } />
-      <Route path="" element={ } />
-      <Route path="" element={ } />
+      <Route path="/" element={Home} />
+      <Route path="/books/create" element={CreateBooks} />
+      <Route path="/books/details/:id" element={ShowBook} />
+      <Route path="/books/edit/:id" element={EditBook} />
+      <Route path="/books/delete/:id" element={DeleteBook} />
     </Routes>
   )
 }
