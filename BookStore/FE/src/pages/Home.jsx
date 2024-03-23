8ad9439d6f1ba90c1e02507e28010aefb4cbddeb
zhorogs <zgslavchev@gmail.com>
2024-03-24 00:08:40 +0200

    configuring home

--- BookStore/FE/src/pages/Home.jsx
+++ BookStore/FE/src/pages/Home.jsx
@@ -6,10 +6,45 @@ import { AiOutlineEdit } from 'react-icons/ai';
 import { BsInfoCircle } from 'react-icons/bs';
 import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
 
-function Home() {
+const Home = () => {
+    const [books, setBooks] = useState([]);
+    const [loading, setLoading] = useState(false);
+    useEffect(() => {
+        setLoading(true);
+        axios
+            .get("http://localhost:5555/books")
+            .then((response) => {
+                setBooks(response.data.data);
+                setLoading(false);
+            })
+            .catch((error) => {
+                console.log(error);
+                setLoading(false);
+            })
+    }, []);
     return (
-        <div>Home</div>
+        <div className="p-4">
+            <div className="flex justify-between items-center">
+                <h1 className="text-3xl my-8">Books List</h1>
+                <Link to="/books/create">
+                    <MdOutlineAddBox className="text-sky-800 text-4xl" />
+                </Link>
+                {loading ? (
+                    <Spinner />
+                ) : (
+                    <table className="w-full border-separate border-spacing-2">
+                        <thead>
+
+                        </thead>
+                        <tbody>
+
+                        </tbody>
+                    </table>
+                )}
+            </div>
+        </div>
     )
-}
+};
+
 
 export default Home
