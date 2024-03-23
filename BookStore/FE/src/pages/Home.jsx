6bc8e480fad79f90de55f991c0dc707f1d4c804f
zhorogs <zgslavchev@gmail.com>
2024-03-23 23:57:17 +0200

    configuring home page

--- BookStore/FE/src/pages/Home.jsx
+++ BookStore/FE/src/pages/Home.jsx
@@ -1,4 +1,10 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import spinner from '../components/spinner';
+import { Link } from 'react-router-dom';
+import { AiOutlineEdit } from 'react-icons/ai';
+import { BsInfoCircle } from 'react-icons/bs';
+import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
 
 function Home() {
     return (
