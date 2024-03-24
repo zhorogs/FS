f918b476a081b07b3829879428bf83f64d9bd241
zhorogs <zgslavchev@gmail.com>
2024-03-25 00:00:52 +0200

    debugging fe error

--- BookStore/FE/src/pages/Home.jsx
+++ BookStore/FE/src/pages/Home.jsx
@@ -50,33 +50,34 @@ const Home = () => {
                         </thead>
                         <tbody>
                             {books.map((book, index) => {
-                                <tr key={book._id} className="h-8">
-                                    <td className="border border-slate-700 rounded-md text-center">
-                                        {index + 1}
-                                    </td>
-                                    <td className="border border-slate-700 rounded-md text-center">
-                                        {book.title}
-                                    </td>
-                                    <td className="border border-slate-700 rounded-md text-center max-md:hidden">
-                                        {book.author}
-                                    </td>
-                                    <td className="border border-slate-700 rounded-md text-center max-md:hidden">
-                                        {book.publishYear}
-                                    </td>
-                                    <td className="border border-slate-700 rounded-md text-center">
-                                        <div className="flex justify-center gap-x-4">
-                                            <Link to={"/books/details/${book._id}"}>
-                                                <BsInfoCircle className="text-2x1 text-green-800"></BsInfoCircle>
-                                            </Link>
-                                            <Link to={"/books/edit/${book._id}"}>
-                                                <AiOutlineEdit className="text-2x1 text-yellow-600"></AiOutlineEdit>
-                                            </Link>
-                                            <Link to="/books/delete/${book._id}">
-                                                <MdOutlineDelete className="text-2x1 text-red-600"></MdOutlineDelete>
-                                            </Link>
-                                        </div>
-                                    </td>
-                                </tr>;
+                                return (
+                                    <tr key={book._id} className="h-8">
+                                        <td className="border border-slate-700 rounded-md text-center">
+                                            {index + 1}
+                                        </td>
+                                        <td className="border border-slate-700 rounded-md text-center">
+                                            {book.title}
+                                        </td>
+                                        <td className="border border-slate-700 rounded-md text-center max-md:hidden">
+                                            {book.author}
+                                        </td>
+                                        <td className="border border-slate-700 rounded-md text-center max-md:hidden">
+                                            {book.publishYear}
+                                        </td>
+                                        <td className="border border-slate-700 rounded-md text-center">
+                                            <div className="flex justify-center gap-x-4">
+                                                <Link to={"/books/details/${book._id}"}>
+                                                    <BsInfoCircle className="text-2x1 text-green-800"></BsInfoCircle>
+                                                </Link>
+                                                <Link to={"/books/edit/${book._id}"}>
+                                                    <AiOutlineEdit className="text-2x1 text-yellow-600"></AiOutlineEdit>
+                                                </Link>
+                                                <Link to="/books/delete/${book._id}">
+                                                    <MdOutlineDelete className="text-2x1 text-red-600"></MdOutlineDelete>
+                                                </Link>
+                                            </div>
+                                        </td>
+                                    </tr>);
                             })}
                         </tbody>
                     </table>
