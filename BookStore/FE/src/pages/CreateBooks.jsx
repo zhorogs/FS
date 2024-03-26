a164a9fdd01f6ac8f46a5fb24b9831d4856ce7ec
zhorogs <zgslavchev@gmail.com>
2024-03-26 23:25:26 +0200

    configure createbook component

--- BookStore/FE/src/pages/CreateBooks.jsx
+++ BookStore/FE/src/pages/CreateBooks.jsx
@@ -10,6 +10,25 @@ function CreateBooks() {
     const [publishYear, setPublishYear] = useState('');
     const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
+    const handleSaveBook = () => {
+        const data = {
+            title,
+            author,
+            publishYear
+        };
+        setLoading(true);
+        axios
+            .post('http://localhost:5555/books', data)
+            .then(() => {
+                setLoading(false);
+                navigate('/');
+            })
+            .catch((error) => {
+                setLoading(false);
+                alert('An error happened. Please check console');
+                console.log(error);
+            })
+    };
 
 
     return (
