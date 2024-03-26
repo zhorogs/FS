ebac6c57ffd07eae886a17ce110b25738595f7b2
zhorogs <zgslavchev@gmail.com>
2024-03-26 23:07:49 +0200

    add imports for showbook

--- BookStore/FE/src/pages/ShowBook.jsx
+++ BookStore/FE/src/pages/ShowBook.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
 
 function ShowBook() {
+
     return (
         <div>ShowBook</div>
     )
