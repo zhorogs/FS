08870a58b46afb80f5e90e174252345e7a721bd9
zhorogs <zgslavchev@gmail.com>
2024-03-27 23:22:51 +0200

    debugging posting error

--- BookStore/BE/models/bookModel.js
+++ BookStore/BE/models/bookModel.js
@@ -11,7 +11,7 @@ const bookSchema = mongoose.Schema(
             type: String,
             required: true
         },
-        publisher: {
+        publishYear: {
             type: Number,
             required: true
         }
