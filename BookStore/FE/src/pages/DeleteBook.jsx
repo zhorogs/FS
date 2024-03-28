eac8c60f54ca2cb079bac135c97068d7c0592244
zhorogs <zgslavchev@gmail.com>
2024-03-29 00:06:14 +0200

    configuring deletebook component

--- BookStore/FE/src/pages/DeleteBook.jsx
+++ BookStore/FE/src/pages/DeleteBook.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react';
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
+import axios from 'axios';
+import { useNavigate, useParams } from 'react-router-dom';
 
 function DeleteBooks() {
-    return (
-        <div>DeleteBooks</div>
-    )
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+    const { id } = useParams();
+    const handleDeleteBook = () => {
+        setLoading(true);
+        axios
+            .delete(`http://localhost:5555/books/${id}`);
+            .then(() => {
+                setLoading(false);
+                navigate('/');
+            })
+        .catch((error) => {
+            setLoading(false);
+            alert('An error happened. Please check console.');
+            console.log(error);
+        });
 }
+return (
+    <div>DeleteBooks</div>
+)
+
 
 export default DeleteBooks
