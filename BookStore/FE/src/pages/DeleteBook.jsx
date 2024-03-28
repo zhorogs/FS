f5bfa044d7c8b27aced64bc8dedf6738c16470bd
zhorogs <zgslavchev@gmail.com>
2024-03-29 00:12:03 +0200

    delete book working pushing

--- BookStore/FE/src/pages/DeleteBook.jsx
+++ BookStore/FE/src/pages/DeleteBook.jsx
@@ -11,20 +11,33 @@ function DeleteBooks() {
     const handleDeleteBook = () => {
         setLoading(true);
         axios
-            .delete(`http://localhost:5555/books/${id}`);
+            .delete(`http://localhost:5555/books/${id}`)
             .then(() => {
                 setLoading(false);
                 navigate('/');
             })
-        .catch((error) => {
-            setLoading(false);
-            alert('An error happened. Please check console.');
-            console.log(error);
-        });
+            .catch((error) => {
+                setLoading(false);
+                alert('An error happened. Please check console.');
+                console.log(error);
+            });
+    }
+    return (
+        <div className='p-4'>
+            <BackButton />
+            <h1 className='text-3xl my-4'>Delete
+                book</h1>
+            {loading ? <Spinner /> : ''}
+            <div className='flex flex-col items-center border-2 border-sky-400 rounded-xl w-[600px] p-8 mx-auto'>
+                <h3 className='text-2xl'>
+                    Are you sure you want to delete this book?
+                </h3>
+                <button className='p-4 bg-red-600 text-white m-8 w-full' onClick={handleDeleteBook}>
+                    Yes, delete it.
+                </button>
+
+            </div>
+        </div>
+    )
 }
-return (
-    <div>DeleteBooks</div>
-)
-
-
-export default DeleteBooks
+export default DeleteBooks;
