f8ade9bdd6ae32b2e8436b37a559c6fb1292807f
zhorogs <zgslavchev@gmail.com>
2024-03-29 22:50:28 +0200

    add buttons table and card

--- BookStore/FE/src/pages/Home.jsx
+++ BookStore/FE/src/pages/Home.jsx
@@ -28,6 +28,14 @@ const Home = () => {
     }, []);
     return (
         <div className="p-4">
+            <div className='flex justify-between items-center'>
+                <button className='bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg' onClick={() => setShowType('table')}>
+                    Table
+                </button>
+                <button className='bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg' onClick={() => setShowType('card')}>
+                    Card
+                </button>
+            </div>
             <div className="flex justify-between items-center">
                 <h1 className="text-3xl my-8">Books List</h1>
                 <Link to="/books/create">
