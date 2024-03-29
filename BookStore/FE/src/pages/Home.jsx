ea3c3994908f80e41eb767e159a08c9adb475dba
zhorogs <zgslavchev@gmail.com>
2024-03-29 22:44:39 +0200

    add bookstable component to home

--- BookStore/FE/src/pages/Home.jsx
+++ BookStore/FE/src/pages/Home.jsx
@@ -35,7 +35,7 @@ const Home = () => {
                 {loading ? (
                     <Spinner />
                 ) : (
-                    
+                    <BooksTable />
                 )}
             </div>
         </div>
