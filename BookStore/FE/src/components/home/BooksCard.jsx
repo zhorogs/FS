7eb375d252514623762af3a946aa94549ced0f47
zhorogs <zgslavchev@gmail.com>
2024-03-29 23:43:30 +0200

    working on bookcards jsx

--- BookStore/FE/src/components/home/BooksCard.jsx
+++ BookStore/FE/src/components/home/BooksCard.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { PiBookOpenTextLight } from 'react-icons/pi';
+import { BiUserCircle } from 'react-icons/bi';
+import { AiOutlineEdit } from 'react-icons/ai';
+import { BsInfoCircle } from 'react-icons/bs';
+import { MdOutlineDelete } from 'react-icons/md';
 
-function BooksCard() {
+
+const BooksCard = ({ books }) => {
     return (
-        <div>BooksCard</div>
+        <div className='grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>BooksCard</div>
     )
 }
 
