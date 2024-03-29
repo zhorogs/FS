159a20677a734a6427ca770e11b02854d9be0361
zhorogs <zgslavchev@gmail.com>
2024-03-29 22:40:19 +0200

    import bookscard and bookstable in homdejsx

--- BookStore/FE/src/pages/Home.jsx
+++ BookStore/FE/src/pages/Home.jsx
@@ -5,6 +5,8 @@ import { Link } from "react-router-dom";
 import { AiOutlineEdit } from "react-icons/ai";
 import { BsInfoCircle } from "react-icons/bs";
 import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md";
+import BooksTable from "../components/home/BooksTable";
+import BooksCard from "../components/home/BooksCard";
 
 
 const Home = () => {
