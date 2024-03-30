2cedb9bcbc74d7973d402c9c85313be23eb18dda
zhorogs <zgslavchev@gmail.com>
2024-03-30 23:42:36 +0200

    add imports to modals

--- BookStore/FE/src/components/home/BookModal.jsx
+++ BookStore/FE/src/components/home/BookModal.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React from 'react';
+import { AiOutlineClose } from 'react-icons/ai';
+import { PiBookOpenTextLight } from 'react-icons/pi';
+import { BiUserCircle } from 'react-icons/bi';
 
-function BookModal() {
+const BookModal = ({ book, onClose }) => {
     return (
         <div>BookModal</div>
     )
