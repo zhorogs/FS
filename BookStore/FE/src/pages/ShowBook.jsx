e03cb4c49b2a5b3b428d218f05ec5993a051325f
zhorogs <zgslavchev@gmail.com>
2024-03-31 00:03:32 +0200

    fix modal

--- BookStore/FE/src/pages/ShowBook.jsx
+++ BookStore/FE/src/pages/ShowBook.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import BackButton from '../components/BackButton';
 import Spinner from '../components/Spinner';
 
-function ShowBook() {
+const ShowBook = () => {
     const [book, setBook] = useState({});
     const [loading, setLoading] = useState(false);
     const { id } = useParams();
@@ -20,8 +20,9 @@ function ShowBook() {
             .catch((error) => {
                 console.log(error);
                 setLoading(false);
-            })
-    })
+            });
+    }, []);
+
     return (
         <div className='p-4'>
             <BackButton />
@@ -45,19 +46,19 @@ function ShowBook() {
                     <div className='my-4'>
                         <span className='text-xl mr-4 text-gray-500'>Publish Year</span>
                         <span>{book.publishYear}</span>
-                        <div className='my-4'>
-                            <span className='text-xl mr-4 text-gray-500'>Create Time</span>
-                            <span>{new Date(book.createdAt).toString()}</span>
-                        </div>
-                        <div className='my-4'>
-                            <span className='text-xl mr-4 text-gray-500'>Last Update Time</span>
-                            <span>{new Date(book.updatedAt).toString()}</span>
-                        </div>
+                    </div>
+                    <div className='my-4'>
+                        <span className='text-xl mr-4 text-gray-500'>Create Time</span>
+                        <span>{new Date(book.createdAt).toString()}</span>
+                    </div>
+                    <div className='my-4'>
+                        <span className='text-xl mr-4 text-gray-500'>Last Update Time</span>
+                        <span>{new Date(book.updatedAt).toString()}</span>
                     </div>
                 </div>
             )}
         </div>
-    )
-}
+    );
+};
 
-export default ShowBook
+export default ShowBook;
