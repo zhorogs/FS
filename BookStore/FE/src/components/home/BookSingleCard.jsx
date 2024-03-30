545c768cdb04c26048963d330c844c13a6b804f0
zhorogs <zgslavchev@gmail.com>
2024-03-30 23:36:49 +0200

    fix singnlebookcard function

--- BookStore/FE/src/components/home/BookSingleCard.jsx
+++ BookStore/FE/src/components/home/BookSingleCard.jsx
@@ -7,7 +7,7 @@ import { BsInfoCircle } from 'react-icons/bs';
 import { MdOutlineDelete } from 'react-icons/md';
 
 
-function BookSingleCard({ book }) {
+const BookSingleCard = ({ book }) => {
     return (
         <div key={item._id} className='border-2 border-gray-500 rounded-lg px-4 py-2 m-4 relative hover:shadow-xl'>
             <h2 className='absolute top-1 right-2 px-4 py-1 bg-red-300 rounded-lg'>
