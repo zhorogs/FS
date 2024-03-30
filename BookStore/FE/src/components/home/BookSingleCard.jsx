e389bde7c9372727dfa4b5de6771e6c11ad4dccd
zhorogs <zgslavchev@gmail.com>
2024-03-30 23:55:53 +0200

    add onclick to show modal

--- BookStore/FE/src/components/home/BookSingleCard.jsx
+++ BookStore/FE/src/components/home/BookSingleCard.jsx
@@ -14,6 +14,7 @@ const BookSingleCard = ({ book }) => {
     return (
         <div className='border-2 border-gray-500 rounded-lg px-4 py-2 m-4 relative hover:shadow-xl'>
             <div className='flex justify-between items-center gap-x-2 mt-4 p-4'>
+                <BiShow className=' text-3xl text-blue-800 hover:text-black cursor-pointer' onClick={() => setShowModal(true)} />
                 <Link to={`/books/details/${book._id}`}>
                     <BsInfoCircle className='text-2xl text-green-800 hover:text-black' />
                 </Link>
