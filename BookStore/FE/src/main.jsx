a91a1190b21ae75998861680553e38fd2ed06a78
zhorogs <zgslavchev@gmail.com>
2024-04-01 23:49:21 +0300

    wrap app with snackbar

--- BookStore/FE/src/main.jsx
+++ BookStore/FE/src/main.jsx
@@ -6,7 +6,10 @@ import { BrowserRouter } from 'react-router-dom';
 import { SnackbarProvider } from 'notistack';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <SnackbarProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </SnackbarProvider>
+
 )
