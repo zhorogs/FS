7ecce258d9c1f6b8fd4e9631995ea9d960b1c3ac
zhorogs <161776956+zhorogs@users.noreply.github.com>
2024-04-02 14:49:02 +0300

    refactoring code

--- BookStore/FE/src/App.jsx
+++ BookStore/FE/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Home from "./pages/Home";
+import Home from './pages/Home';
 import CreateBooks from './pages/CreateBooks';
 import DeleteBook from './pages/DeleteBook';
 import EditBooks from './pages/EditBooks';
