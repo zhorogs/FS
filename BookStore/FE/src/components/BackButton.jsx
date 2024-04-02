778005a1e4e08c90631fae37fa6bf46a327ae140
zhorogs <161776956+zhorogs@users.noreply.github.com>
2024-04-02 14:55:07 +0300

    refactor backbutton component

--- BookStore/FE/src/components/BackButton.jsx
+++ BookStore/FE/src/components/BackButton.jsx
@@ -1,16 +1,14 @@
 import { Link } from 'react-router-dom';
 import { BsArrowLeft } from 'react-icons/bs';
 
-
-
-const BackButton = ({ destination = "/" }) => {
+const BackButton = ({ destination = '/' }) => {
     return (
-        <div className="flex">
-            <Link to={destination} className="bg-sky-800 text-white px-4 w-fit">
-                <BsArrowLeft className="text-2xl" />
+        <div className='flex'>
+            <Link to={destination} className='bg-sky-800 text-white px-4 w-fit'>
+                <BsArrowLeft className='text-2xl' />
             </Link>
         </div>
     )
 }
 
-export default BackButton
+export default BackButton;
