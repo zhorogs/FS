f4aa9b01cfeee96d7be424f6fbbd8e5358272d7c
zhorogs <161776956+zhorogs@users.noreply.github.com>
2024-04-02 14:48:43 +0300

    refactoring code

--- BookStore/FE/src/App.jsx
+++ BookStore/FE/src/App.jsx
@@ -1,19 +1,19 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home";
-import CreateBooks from "./pages/CreateBooks";
-import DeleteBook from "./pages/DeleteBook";
-import EditBooks from "./pages/EditBooks";
-import ShowBook from "./pages/ShowBook";
+import CreateBooks from './pages/CreateBooks';
+import DeleteBook from './pages/DeleteBook';
+import EditBooks from './pages/EditBooks';
+import ShowBook from './pages/ShowBook';
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/books/create" element={<CreateBooks />} />
-      <Route path="/books/details/:id" element={<ShowBook />} />
-      <Route path="/books/edit/:id" element={<EditBooks />} />
-      <Route path="/books/delete/:id" element={<DeleteBook />} />
+      <Route path='/' element={<Home />} />
+      <Route path='/books/create' element={<CreateBooks />} />
+      <Route path='/books/details/:id' element={<ShowBook />} />
+      <Route path='/books/edit/:id' element={<EditBooks />} />
+      <Route path='/books/delete/:id' element={<DeleteBook />} />
     </Routes>
   )
 }
