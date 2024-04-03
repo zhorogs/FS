f6c2b0ace5d4c167b291e5e5c74c5c05eb152485
zhorogs <zgslavchev@gmail.com>
2024-04-03 23:41:22 +0300

    removing not needed code

--- BookStore/FE/src/components/home/BookSingleCard.jsx
+++ BookStore/FE/src/components/home/BookSingleCard.jsx
@@ -1,7 +1,5 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
-import { PiBookOpenTextLight } from 'react-icons/pi';
-import { BiUserCircle, BiShow } from 'react-icons/bi';
+import { BiShow } from 'react-icons/bi';
 import { AiOutlineEdit } from 'react-icons/ai';
 import { BsInfoCircle } from 'react-icons/bs';
 import { MdOutlineDelete } from 'react-icons/md';
