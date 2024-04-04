0c2ca2ff58ed034fafbcf4221d6619ad8dc3f62f
zhorogs <zgslavchev@gmail.com>
2024-04-04 23:33:59 +0300

    add mongo db connection and configure schema

--- MERNLEARN/BackEnd/index.js
+++ MERNLEARN/BackEnd/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb + srv://zgslavchev:dGJ5Ihk9EIi9sNdN@cluster0.2jj9g8e.mongodb.net/');
 
 app.listen(3001, () => {
     console.log('Server runs!');
---
+++ MERNLEARN/BackEnd/models/Users.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    }
+})
